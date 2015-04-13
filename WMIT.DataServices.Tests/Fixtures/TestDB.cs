fda9078dc588ffaea2d89585f8f59a4401a83092
kpko <plastocyst@googlemail.com>
2015-04-13 15:18:15 +0200

    Added test database to tests using Effort

---
+++ WMIT.DataServices.Tests/Fixtures/TestDB.cs
@@ -0,0 +1,47 @@
+﻿using Effort;
+using Effort.DataLoaders;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WMIT.DataServices.Model;
+
+namespace WMIT.DataServices.Tests
+{
+    class Contact : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class TestDB : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public TestDB(DbConnection connection) : base(connection, true)
+        { 
+        }
+
+        static CachingDataLoader cachingDataLoader = null;
+        public static TestDB Create()
+        {
+            if (cachingDataLoader == null)
+            {
+                // TODO: implement fixture data
+                var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..\\..\\Data"); 
+                var csvDataLoader = new CsvDataLoader(path);
+                cachingDataLoader = new CachingDataLoader(csvDataLoader);
+            }
+
+            var connection = DbConnectionFactory.CreateTransient(cachingDataLoader);
+            var db = new TestDB(connection);
+
+            return db;
+        }
+    }
+}
